7f389b8aa826e798fee47c41dac2e6d0442cec0a
savanahamood <savanahamood@gmail.com>
2023-04-18 03:04:13 +0000

    regex

---
+++ regex.test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+
+
+/* Write a function that take a string and return true if the string only contain uppercase and lowercase
+characters (no numbers and symbols) and it should end with capital A else return false */
+
+function capitalA(s){
+   
+ // Add your logic.
+ let a =/\A$/
+ let result = a.test(s);     
+    return result ;
+
+}
+
+
+/* Write a function that take a string and return true if the the sting is following the emails pattern
+which end with io (example@example.io) */
+
+function ioEmail(email){
+    // Add your logic.
+  /*   let b =/.io$/;
+  let n=/@/;
+ let result1 = b.test(email);   
+//  console.log(result1)
+ let result2 = n.test(email);     
+return result1&&result2 */
+// let valid= /^[a-zA-Z][^0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/+\.io$/;
+//let valid=/^[a-zA-Z]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(io)$/;
+/* let valid=/\w+@\w+\.+io$/;
+    return valid.test(email); */
+    let valid=/^[\w]+@([\w]+\.io$)/;
+    return valid.test(email);}
+   
+
+/* function ioEmail(email){
+    let valid= /^[A-Za-z]+@[A-Za-z]+\.io$/;
+       return valid.test(email)
+   } */
+/* You have a text that contain image names with their extention you need to write a function to 
+find all images in that text and return their names and extention in an array 
+required extention are jpg, jpeg and png.
+*/
+
+function imagesSearcher(text){
+    let images = [];
+    // Add your logic.
+    let words = text.split(" ")
+    words.forEach(word => {
+        if(word.match(/\w+\.+(png|jpg|jpeg)/)){
+        images.push(word)}})
+        return images
+}
+
+
+describe("Test capitalA", ()=>{
+    test("It should return true if the input has uppercase and lowercase characters (no numbers and symbols) and it should end with capital A else return false ", () => {
+        expect(capitalA("Hello world A")).toStrictEqual(true);
+
+        expect(capitalA("Hello world")).toStrictEqual(false);
+
+        expect(capitalA("Hello world a")).toStrictEqual(false);
+    })
+});
+
+describe("Test ioEmail", () => {
+    test("It should return true if the input is in email format that end with .io", () => {
+        expect(ioEmail("example@example.io")).toStrictEqual(true);
+        expect(ioEmail("ex@ample@example.io")).toStrictEqual(false);
+        expect(ioEmail("ex.ample@example.io")).toStrictEqual(false);
+        expect(ioEmail("example@example.gmail")).toStrictEqual(false);
+    })
+});
+
+
+describe("Test imagesSearcher", () => {
+    test("It should return all images names that end with jpg, jpeg and png extention", () => {
+        expect(imagesSearcher("Lorem ipsum dolor sit amet, consectetur adipiscing elit, cat.png sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. dog.jpg Ut enim ad minim veniam, quis nostrud exercitation ullamco cow.jpeg laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")).toStrictEqual(['cat.png', 'dog.jpg', 'cow.jpeg']);
+        expect(imagesSearcher("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")).toStrictEqual([]);
+        expect(imagesSearcher("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. horse.gif Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore dolphin.pdf eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa mouse.tiff qui officia deserunt mollit anim id est laborum.")).toStrictEqual([]);
+    })
+})
